fafb3f25e333a4c4eeed25c4043b89f8536f9c9e
Usman Bashir <me@usmanbashir.com>
2019-12-05 14:56:12 +0300

    Add JSON Middleware

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ const app = express();
 // Define PORT for the API to run on
 const port = process.env.PORT || 5000;
 
+/*** Middleware ***/
+
+// Add `bodyParser` middleware which will parse JSON requests into
+// JS objects before they reach the route files.
+//
+// The method `.use` sets up middleware for the Express application
+app.use(express.json());
+
 /*** Routes ***/
 
 // Mount imported Routers
