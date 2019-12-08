38376d73cdc40b9aeaffbd3ed9326ae31fa51c52
Usman Bashir <me@usmanbashir.com>
2019-12-08 10:28:48 +0300

    Add CORS Support

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Require necessary NPM Packages
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Require Route Files
 const indexRouter = require('./app/routes/index');
@@ -20,6 +21,7 @@ const app = express();
 
 // Define PORT for the API to run on
 const port = process.env.PORT || 5000;
+const reactPort = 3000;
 
 /*** Middleware ***/
 
@@ -28,6 +30,9 @@ const port = process.env.PORT || 5000;
 //
 // The method `.use` sets up middleware for the Express application
 app.use(express.json());
+
+// Set CORS headers on response from this API using the `cors` NPM package
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}` }));
 
 /*** Routes ***/
 
