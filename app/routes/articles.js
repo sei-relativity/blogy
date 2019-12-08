a443c86f13659c169701cf684069f2ae847b02e4
Usman Bashir <me@usmanbashir.com>
2019-12-08 09:39:16 +0300

    Add Update Article Route

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -77,6 +77,31 @@ router.post('/api/articles', (req, res) => {
 * URI:          /api/articles/5d664b8b68b4f5092aba18e9
 * Description:  Update An Article by Article ID
  */
+router.patch('/api/articles/:id', function(req, res) {
+  Article.findById(req.params.id)
+    .then(function(article) {
+      if(article) {
+        // Pass the result of Mongoose's `.update` method to the next `.then`
+        return article.update(req.body.article);
+      } else {
+        // If we couldn't find a document with the matching ID
+        res.status(404).json({
+          error: {
+            name: 'DocumentNotFoundError',
+            message: 'The provided ID doesn\'t match any documents'
+          }
+        });
+      }
+    })
+    .then(function() {
+      // If the update succeeded, return 204 and no JSON
+      res.status(204).end();
+    })
+    // Catch any errors that might occur
+    .catch(function(error) {
+      res.status(500).json({ error: error });
+    });
+});
 
 /**
  * Action:      DESTROY
